85791c09d913f298a13b5315bb310823ca5867e1
Steve Bennett <stevage@gmail.com>
2016-04-16 15:51:18 +1000

    Initial

---
+++ tutors.js
@@ -0,0 +1,71 @@
+window.tutors = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Arabic",
+        "description": "My description!"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.9539566040039,
+          -37.8127675575702
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Arabic"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.96631622314453,
+          -37.82144644892643
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Arabic"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.97695922851562,
+          -37.78808138412046
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Farsi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.95086669921875,
+          -37.76474401178003
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "language": "Farsi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          144.9199676513672,
+          -37.785910776551354
+        ]
+      }
+    }
+  ]
+};
